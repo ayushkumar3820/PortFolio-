542a189aa5902b87801376fee82515fbe9efe5df
Your Name <you@example.com>
2025-01-13 16:41:21 +0530

    edit

--- components/commonHead/index.js
+++ components/commonHead/index.js
@@ -5,7 +5,7 @@ import Head from 'next/head'
 const CommonHead = (props) => {
     return (
         <Head>
-            <title>Elito - Creative Portfolio Next Template</title>
+            <title>Ayush Kr</title>
             <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossOrigin="anonymous"></link>
             <link rel="preconnect" href="https://fonts.googleapis.com" />
             <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
