8fc5b1bfdcabeb28350b12943f90d2e00c0b178b
ayushkumar3820 <kumarayush382000@gamil.com>
2025-08-10 23:04:49 +0530

    add new company exper.

--- components/Exprience/Exprience.js
+++ components/Exprience/Exprience.js
@@ -2,42 +2,55 @@ import React from 'react'
 import Link from 'next/link'
 import SectionTitle from '../SectionTitle/SectionTitle'
 
-const Expriences = [
-    
+const Experiences = [
     {
-        date: ' May/2023- Present ',
-        logo: 'images/Fitelologo.png',
+        date: 'Feb/2025 - Present',
+        logo: 'https://weboum.com/image/weboum-footer-logo-1.png', 
         position: 'Software Developer Engineer-1',
-        companyName: ' Fitelo (No Guilt Fitness & Nutrition India Pvt. Ltd.),Mohail,punjab',
-        workFrom: ',Hybrid'
-        ,
+        companyName: 'Weboum Technologies Pvt. Ltd., Mohali, Punjab',
+        workFrom: 'On-site',
+        website: 'https://weboum.com/'
     },
     {
-        date: 'Oct/2023 - May/2023',
-        logo: 'images/Fitelologo.png',
+        date: 'May/2024 - Jan/2025',
+        logo: '/images/Fitelologo.png',
+        position: 'Software Developer Engineer-1',
+        companyName: 'Fitelo (No Guilt Fitness & Nutrition India Pvt. Ltd.), Mohali, Punjab',
+        workFrom: 'On-site/Hybrid',
+        website: 'https://fitelo.co'
+    },
+    {
+        date: 'Oct/2023 - May/2024',
+        logo: '/images/Fitelologo.png',
         position: 'Quality Analyst Intern',
-        companyName: 'Fitelo (No Guilt Fitness & Nutrition India Pvt. Ltd.),punjab',
-        workFrom: ',Hybrid',
+        companyName: 'Fitelo (No Guilt Fitness & Nutrition India Pvt. Ltd.), Punjab',
+        workFrom: 'On-site/Hybrid',
+        website: 'https://fitelo.co'
     }
-    
-
 ]
 
-
-const ExprienceSec = (props) => {
+const ExperienceSec = () => {
     return (
         <div className="wpo-work-area section-padding">
             <div className="container">
                 <SectionTitle Title={'My Work Experience'} />
                 <div className="wpo-work-wrap">
-                    {Expriences.map((exprience, exp) => (
-                        <div className="wpo-work-item" key={exp}>
+                    {Experiences.map((experience, index) => (
+                        <div className="wpo-work-item" key={index}>
                             <ul>
-                                <li className="date">{exprience.date}</li>
-                                <li className="logo"><img src={exprience.logo} alt="" /></li>
-                                <li className="position">{exprience.position} <span>{exprience.companyName} <span>{exprience.workFrom}</span></span></li>
+                                <li className="date">{experience.date}</li>
+                                <li className="logo">
+                                    <img src={experience.logo} alt={experience.companyName} />
+                                </li>
+                                <li className="position">
+                                    {experience.position}{' '}
+                                    <span>
+                                        {experience.companyName}{' '}
+                                        <span>({experience.workFrom})</span>
+                                    </span>
+                                </li>
                                 <li className="link">
-                                    <Link href="https://fitelo.co/">
+                                    <Link href={experience.website} target="_blank">
                                         Go to website
                                     </Link>
                                 </li>
@@ -52,8 +65,15 @@ const ExprienceSec = (props) => {
                         <circle cx="750" cy="750" r="200" />
                     </g>
                     <defs>
-                        <filter id="filter0_f_39_4214" x="0" y="0" width="1500" height="1500"
-                            filterUnits="userSpaceOnUse" colorInterpolationFilters="sRGB">
+                        <filter
+                            id="filter0_f_39_4214"
+                            x="0"
+                            y="0"
+                            width="1500"
+                            height="1500"
+                            filterUnits="userSpaceOnUse"
+                            colorInterpolationFilters="sRGB"
+                        >
                             <feFlood floodOpacity="0" result="BackgroundImageFix" />
                             <feBlend mode="normal" in="SourceGraphic" in2="BackgroundImageFix" result="shape" />
                             <feGaussianBlur stdDeviation="275" result="effect1_foregroundBlur_39_4212" />
@@ -65,4 +85,4 @@ const ExprienceSec = (props) => {
     )
 }
 
-export default ExprienceSec;
+export default ExperienceSec
